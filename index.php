8a3ccfb940ae810029157a3befa373c81de11008
Aaron Akers <aja.2013@yahoo.com>
2021-07-27 09:37:09 -0400

    start of week5
    Added fail, check, and index .php stuff

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Aaron Akers PHP<\title>
+    <\head>
+    <body>
+        <h1>Aaron Akers PHP<\h1>
+        <pre>       
+                    xxx
+                   xx xx
+                  xx   xx
+                 xx     xx
+                xxxxxxxxxxx
+               xx         xx
+              xx           xx
+             xx             xx
+            xx               xx
+         
+        <\pre>
+        
+        <?php
+            print hash('sha256','Aaron Akers);
+        ?>
+    <\body>
+<\html>
