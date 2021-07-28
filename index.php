e95c8ca60952fdbb7a593b233b3d3c56a98c8e68
AkersA <85839016+AkersA@users.noreply.github.com>
2021-07-27 21:06:52 -0400

    final copy

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Aaron Akers PHP<\title>
-    <\head>
+        <title>Aaron Akers PHP</title>
+    </head>
     <body>
-        <h1>Aaron Akers PHP<\h1>
+        <h1>Aaron Akers PHP</h1>
         <pre>       
                     xxx
                    xx xx
@@ -16,10 +16,14 @@
              xx             xx
             xx               xx
          
-        <\pre>
+        </pre>
         
         <?php
-            print hash('sha256','Aaron Akers);
+            print hash('sha256','Aaron Akers');
         ?>
-    <\body>
-<\html>
+        <br>
+        <a href="check.php">check error settings</a>
+        <br>
+        <a href="fail.php">failure message</a>
+    </body>
+</html>
